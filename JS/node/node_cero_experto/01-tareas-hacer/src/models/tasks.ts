61d6eacb34d670cac072e96495f6ff0c7fcc6b53
Jorge Rodríguez Sánchez <jorosadev@gmail.com>
2022-10-31 23:28:50 +0000

    wtf

--- JS/node/node_cero_experto/01-tareas-hacer/src/models/tasks.ts
+++ JS/node/node_cero_experto/01-tareas-hacer/src/models/tasks.ts
@@ -1,17 +1,29 @@
+import { writeFile } from "node:fs/promises";
+import fs from "fs";
 import { Task } from "./task";
+import "colors";
 type List = {
-  [k: Task['id']]: Task;
-}
+  [k: Task["id"]]: Task;
+};
 
 export class Tasks {
-  constructor(private list: List) {
-    this.list = {}
-  }
+  private dir = "./data";
+  private file = "tasks.json";
+  constructor(private list: {}) {}
 
+  public async crearTarea(description: string) {
+    try {
+      const tarea = new Task(description);
 
-  public crearTarea(description: string) {
-    const tarea = new Task(description);
+      this.list = { ...this.list, [tarea.taskId]: tarea };
 
-    this.list = { ...this.list, [tarea.taskId]: tarea }
+      if (!fs.existsSync(this.dir)) {
+        fs.mkdirSync(this.dir);
+      }
+
+      await writeFile(this.dir + this.file, JSON.stringify(this.list));
+    } catch (err) {
+      console.log(err.message.red);
+    }
   }
 }
